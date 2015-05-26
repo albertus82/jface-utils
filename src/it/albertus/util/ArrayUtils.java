d00983470ea8a75a7703840e13ce8ea76241f536
Albertus82 <albertus82@gmail.com>
2015-05-26 18:48:11 +0200

    ArrayUtils.

---
+++ src/it/albertus/util/ArrayUtils.java
@@ -0,0 +1,41 @@
+package it.albertus.util;
+
+public class ArrayUtils {
+
+	public static boolean isNotEmpty(boolean[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(byte[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(short[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(char[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(int[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(long[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(float[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(double[] array) {
+		return (array != null && array.length != 0);
+	}
+
+	public static boolean isNotEmpty(Object[] array) {
+		return (array != null && array.length != 0);
+	}
+
+}

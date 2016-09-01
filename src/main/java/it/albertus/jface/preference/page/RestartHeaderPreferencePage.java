a0c91d92852eabf2943a015865ec0b3ec7314f0e
Albertus82 <albertus82@gmail.com>
2016-09-01 23:12:58 +0200

    Comment.

--- src/main/java/it/albertus/jface/preference/page/RestartHeaderPreferencePage.java
+++ src/main/java/it/albertus/jface/preference/page/RestartHeaderPreferencePage.java
@@ -11,6 +11,7 @@ public class RestartHeaderPreferencePage extends BasePreferencePage {
 
 	@Override
 	protected Control createHeader() {
+		// return createNoteComposite(null, getFieldEditorParent(), JFaceResources.get("lbl.preferences.header.restart"), "");
 		final Label header = new Label(getFieldEditorParent(), SWT.WRAP);
 		TextFormatter.setBoldFontStyle(header);
 		header.setText(JFaceResources.get("lbl.preferences.header.restart"));

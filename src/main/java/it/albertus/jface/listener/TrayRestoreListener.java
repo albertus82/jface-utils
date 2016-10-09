5d28f0d7f44afe80775054b40c64a14d8cfbc776
Albertus82 <albertus82@gmail.com>
2016-10-09 11:26:43 +0200

    Javadoc.

--- src/main/java/it/albertus/jface/listener/TrayRestoreListener.java
+++ src/main/java/it/albertus/jface/listener/TrayRestoreListener.java
@@ -8,22 +8,24 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TrayItem;
 
 /**
- * Listener that restores the shell and optionally hides the tray icon. You can
- * add this listener to both {@link org.eclipse.swt.widgets.Shell Shell} and
- * {@link org.eclipse.swt.widgets.TrayItem TrayItem}.
+ * Listener that restores the shell and optionally hides the tray icon. You
+ * should add this listener to both {@link org.eclipse.swt.widgets.Shell Shell}
+ * and {@link org.eclipse.swt.widgets.TrayItem TrayItem} widgets.
  */
 public class TrayRestoreListener extends ShellAdapter implements Listener {
 
 	private final Shell shell;
 	private final TrayItem trayItem;
 
-	/** Create a listener that restores shell and hides tray icon. */
+	/** Create a listener that restores the shell and hides the tray icon. */
 	public TrayRestoreListener(final Shell shell, final TrayItem trayItem) {
 		this.shell = shell;
 		this.trayItem = trayItem;
 	}
 
-	/** Create a listener that restores shell without hiding tray icon. */
+	/**
+	 * Create a listener that restores the shell without hiding the tray icon.
+	 */
 	public TrayRestoreListener(final Shell shell) {
 		this(shell, null);
 	}

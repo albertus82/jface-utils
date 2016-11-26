aa0b55a9e6e2be970cd341aa75c9d716c1aaf3a6
Albertus82 <albertus82@gmail.com>
2016-11-26 10:18:25 +0100

    Added some methods to StringUtils.

--- src/main/java/it/albertus/util/StringUtils.java
+++ src/main/java/it/albertus/util/StringUtils.java
@@ -2,12 +2,49 @@ package it.albertus.util;
 
 public class StringUtils {
 
+	public static boolean isNotEmpty(final String str) {
+		return str != null && str.length() != 0;
+	}
+
 	public static boolean isNotBlank(final String str) {
 		return str != null && str.trim().length() != 0;
 	}
 
 	public static String trimToEmpty(final String str) {
 		return str != null ? str.trim() : "";
+	}
+
+	public static boolean isNumeric(final String str) {
+		if (str == null) {
+			return false;
+		}
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c < '0' || c > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static String substringAfter(final String str, final String separator) {
+		if (str == null || str.length() == 0) {
+			return str;
+		}
+		if (separator == null || str.indexOf(separator) == -1) {
+			return "";
+		}
+		return str.substring(str.indexOf(separator) + separator.length());
+	}
+
+	public static String substringBefore(final String str, final String separator) {
+		if (str == null || str.length() == 0) {
+			return str;
+		}
+		if (separator == null || str.indexOf(separator) == -1) {
+			return str;
+		}
+		return str.substring(0, str.indexOf(separator));
 	}
 
 }

735b2fab6dc4153f3b40902a516f7f27e33bde44
Albertus82 <albertus82@gmail.com>
2016-11-27 13:05:49 +0100

    StringUtils.

--- src/main/java/it/albertus/util/StringUtils.java
+++ src/main/java/it/albertus/util/StringUtils.java
@@ -2,16 +2,20 @@ package it.albertus.util;
 
 public class StringUtils {
 
+	public static boolean isEmpty(final String str) {
+		return !isNotEmpty(str);
+	}
+
 	public static boolean isNotEmpty(final String str) {
 		return str != null && str.length() != 0;
 	}
 
-	public static boolean isNotBlank(final String str) {
-		return str != null && str.trim().length() != 0;
+	public static boolean isBlank(final String str) {
+		return !isNotBlank(str);
 	}
 
-	public static String trimToEmpty(final String str) {
-		return str != null ? str.trim() : "";
+	public static boolean isNotBlank(final String str) {
+		return str != null && str.trim().length() != 0;
 	}
 
 	public static boolean isNumeric(final String str) {
@@ -19,12 +23,22 @@ public class StringUtils {
 			return false;
 		}
 		for (int i = 0; i < str.length(); i++) {
-			char c = str.charAt(i);
+			final char c = str.charAt(i);
 			if (c < '0' || c > '9') {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	public static String substringBefore(final String str, final String separator) {
+		if (str == null || str.length() == 0) {
+			return str;
+		}
+		if (separator == null || str.indexOf(separator) == -1) {
+			return str;
+		}
+		return str.substring(0, str.indexOf(separator));
 	}
 
 	public static String substringAfter(final String str, final String separator) {
@@ -37,14 +51,8 @@ public class StringUtils {
 		return str.substring(str.indexOf(separator) + separator.length());
 	}
 
-	public static String substringBefore(final String str, final String separator) {
-		if (str == null || str.length() == 0) {
-			return str;
-		}
-		if (separator == null || str.indexOf(separator) == -1) {
-			return str;
-		}
-		return str.substring(0, str.indexOf(separator));
+	public static String trimToEmpty(final String str) {
+		return str != null ? str.trim() : "";
 	}
 
 }

f27bd6119a1e03928990968e5f01a5a24538e7ce
Alberto <albertus82@gmail.com>
2017-01-30 10:05:42 +0100

    Improved localization of ErrorDialog.

--- src/main/java/it/albertus/jface/EnhancedErrorDialog.java
+++ src/main/java/it/albertus/jface/EnhancedErrorDialog.java
@@ -12,7 +12,12 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 
 import it.albertus.util.ExceptionUtils;
@@ -52,6 +57,55 @@ public class EnhancedErrorDialog extends ErrorDialog {
 		super.configureShell(shell);
 		if (images != null && images.length > 0) {
 			shell.setImages(images);
+		}
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(final Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, JFaceMessages.get("lbl.button.ok"), true);
+		createDetailsButton(parent);
+	}
+
+	@Override
+	protected void createDetailsButton(final Composite parent) {
+		super.createDetailsButton(parent); // default button creation
+		if (shouldShowDetailsButton()) {
+			final Button detailsButton = getButton(IDialogConstants.DETAILS_ID);
+			localizeDetailsButton(detailsButton);
+			detailsButton.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(final SelectionEvent se) {
+					localizeDetailsButton(detailsButton);
+				}
+			});
+		}
+	}
+
+	@Override
+	protected void buttonPressed(final int id) {
+		if (id == IDialogConstants.DETAILS_ID) {
+			final Button detailsButton = getButton(id);
+			if (detailsButton != null && !detailsButton.isDisposed()) {
+				detailsButton.setRedraw(false); // eases the localization
+			}
+			super.buttonPressed(id);
+			if (detailsButton != null && !detailsButton.isDisposed()) {
+				detailsButton.setRedraw(true);
+			}
+		}
+		else {
+			super.buttonPressed(id); // default behaviour
+		}
+	}
+
+	protected void localizeDetailsButton(final Button detailsButton) {
+		if (!detailsButton.isDisposed()) {
+			if (IDialogConstants.SHOW_DETAILS_LABEL.equals(detailsButton.getText())) {
+				detailsButton.setText(JFaceMessages.get("lbl.button.details.show"));
+			}
+			else if (IDialogConstants.HIDE_DETAILS_LABEL.equals(detailsButton.getText())) {
+				detailsButton.setText(JFaceMessages.get("lbl.button.details.hide"));
+			}
 		}
 	}
 

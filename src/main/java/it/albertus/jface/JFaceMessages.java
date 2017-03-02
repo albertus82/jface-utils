36afa64e1581484bfcee155c4d769ac8eb63cb00
Alberto <albertus82@gmail.com>
2017-03-02 22:22:18 +0100

    I18N: now client apps can add languages to JFaceMessages.

--- src/main/java/it/albertus/jface/JFaceMessages.java
+++ src/main/java/it/albertus/jface/JFaceMessages.java
@@ -17,51 +17,32 @@ public class JFaceMessages {
 		throw new IllegalAccessError();
 	}
 
-	public enum Language {
-		ENGLISH(Locale.ENGLISH),
-		ITALIAN(Locale.ITALIAN);
-
-		private final Locale locale;
-
-		private Language(final Locale locale) {
-			this.locale = locale;
-		}
-
-		public Locale getLocale() {
-			return locale;
-		}
-	}
-
-	/** Aggiorna la lingua in cui vengono mostrati i messaggi. */
-	public static void setLanguage(final Language language) {
-		if (language != null) {
-			resources = ResourceBundle.getBundle(BASE_NAME, language.locale, ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES));
-		}
-	}
-
-	/** Aggiorna la lingua in cui vengono mostrati i messaggi. */
+	/**
+	 * Switch the language for JFaceUtils messages.
+	 *
+	 * @param language lowercase two-letter ISO-639 code.
+	 *
+	 * @exception NullPointerException thrown if argument is null.
+	 *
+	 * @see java.util.Locale#Locale(String)
+	 */
 	public static void setLanguage(final String language) {
 		if (language != null) {
 			resources = ResourceBundle.getBundle(BASE_NAME, new Locale(language), ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES));
 		}
 	}
 
-	public static Language getLanguage() {
-		for (final Language language : Language.values()) {
-			if (language.locale.equals(resources.getLocale())) {
-				return language;
-			}
-		}
-		return Language.ENGLISH; // Default.
-	}
-
 	public static String get(final String key, final Object... params) {
 		final List<String> stringParams = new ArrayList<String>(params.length);
 		for (final Object param : params) {
-			stringParams.add(param != null ? param.toString() : "");
+			stringParams.add(String.valueOf(param));
 		}
 		final String message = MessageFormat.format(resources.getString(key), stringParams.toArray());
 		return message != null ? message.trim() : "";
+	}
+
+	public static ResourceBundle getResources() {
+		return resources;
 	}
 
 }

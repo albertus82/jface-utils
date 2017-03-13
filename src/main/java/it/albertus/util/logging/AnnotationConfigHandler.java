4ca5fdc9e37c9492029b76f0f2b9023b0cdca167
Alberto <albertus82@gmail.com>
2017-03-13 16:21:15 +0100

    AnnotationConfigHandler

--- src/main/java/it/albertus/util/logging/AnnotationConfigHandler.java
+++ src/main/java/it/albertus/util/logging/AnnotationConfigHandler.java
@@ -3,9 +3,7 @@ package it.albertus.util.logging;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
-import it.albertus.util.AnnotationConfigurationException;
 import it.albertus.util.logging.annotation.ExcludeLoggers;
-import it.albertus.util.logging.annotation.LogFilter;
 import it.albertus.util.logging.annotation.LogFormat;
 
 public abstract class AnnotationConfigHandler extends Handler {
@@ -26,17 +24,6 @@ public abstract class AnnotationConfigHandler extends Handler {
 		final LogFormat format = getClass().getAnnotation(LogFormat.class);
 		if (format != null && format.value() != null) {
 			setFormatter(new CustomFormatter(format.value()));
-		}
-
-		// Filter
-		final LogFilter filter = getClass().getAnnotation(LogFilter.class);
-		if (filter != null && filter.value() != null) {
-			try {
-				setFilter(filter.value().newInstance());
-			}
-			catch (final Exception e) {
-				throw new AnnotationConfigurationException(e);
-			}
 		}
 	}
 

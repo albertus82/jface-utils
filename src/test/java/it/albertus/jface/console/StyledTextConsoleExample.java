b33fe2a4482a8cbe349f45766fe35e80a6d23757
Alberto <albertus82@gmail.com>
2017-04-17 12:51:08 +0200

    ScrollableConsole: improved examples.

--- src/test/java/it/albertus/jface/console/StyledTextConsoleExample.java
+++ src/test/java/it/albertus/jface/console/StyledTextConsoleExample.java
@@ -6,14 +6,21 @@ import org.eclipse.swt.widgets.Display;
 
 public class StyledTextConsoleExample extends ScrollableConsoleExample<StyledText> {
 
+	private static int limit = 10000;
+
 	public static void main(final String... args) {
+		if (args.length > 0) {
+			limit = Integer.parseInt(args[0]);
+		}
 		Display.setAppName(StyledTextConsole.class.getSimpleName() + " Example");
 		new StyledTextConsoleExample().run();
 	}
 
 	@Override
 	protected ScrollableConsole<StyledText> createScrollableConsole(final Composite parent) {
-		return new StyledTextConsole(parent, null, true);
+		final StyledTextConsole stc = new StyledTextConsole(parent, null, true);
+		stc.setLimit(limit);
+		return stc;
 	}
 
 }
--- src/test/java/it/albertus/jface/console/TextConsoleExample.java
+++ src/test/java/it/albertus/jface/console/TextConsoleExample.java
@@ -6,14 +6,21 @@ import org.eclipse.swt.widgets.Text;
 
 public class TextConsoleExample extends ScrollableConsoleExample<Text> {
 
+	private static int limit = 10000;
+
 	public static void main(final String... args) {
+		if (args.length > 0) {
+			limit = Integer.parseInt(args[0]);
+		}
 		Display.setAppName(TextConsole.class.getSimpleName() + " Example");
 		new TextConsoleExample().run();
 	}
 
 	@Override
 	protected ScrollableConsole<Text> createScrollableConsole(final Composite parent) {
-		return new TextConsole(parent, null, true);
+		final TextConsole tc = new TextConsole(parent, null, true);
+		tc.setLimit(limit);
+		return tc;
 	}
 
 }

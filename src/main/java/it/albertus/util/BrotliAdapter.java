5cf9144d3f2153a5a87599e29873a141e4806817
Alberto <albertus82@gmail.com>
2017-09-03 17:15:54 +0200

    Brotli adapter.

--- src/main/java/it/albertus/util/BrotliAdapter.java
+++ src/main/java/it/albertus/util/BrotliAdapter.java
@@ -5,35 +5,77 @@ import haxe.lang.EmptyObject;
 import haxe.root.Array;
 import haxe.root.Brotli;
 
+/**
+ * Adapter class for <b>BrotliHaxe</b> library.
+ * 
+ * @see <a href="https://github.com/dominikhlbg/BrotliHaxe">BrotliHaxe</a>
+ */
 public class BrotliAdapter {
 
-	private final Brotli instance = new Brotli(EmptyObject.EMPTY);
+	/** Minimum Brotli compression quality. */
+	public static final int MIN_QUALITY = 0;
 
+	/** Maximum Brotli compression quality. */
+	public static final int MAX_QUALITY = 11;
+
+	private final Brotli brotli;
+
+	/** Construct a {@code BrotliAdapter} using the default dictionary. */
+	public BrotliAdapter() {
+		brotli = new Brotli(EmptyObject.EMPTY);
+	}
+
+	/**
+	 * Construct a {@code BrotliAdapter} using a custom dictionary.
+	 * 
+	 * @param dictionaryPath the full path of the dictionary file.
+	 */
+	public BrotliAdapter(final String dictionaryPath) {
+		brotli = new Brotli(dictionaryPath);
+	}
+
+	/**
+	 * Compress a byte array using <b>Brotli</b> compression format.
+	 * 
+	 * @param uncompressed the byte array to compress
+	 * @param quality the compression quality, valid range is
+	 *        {@link #MIN_QUALITY} to {@link #MAX_QUALITY}
+	 * @return the compressed byte array.
+	 */
 	@SuppressWarnings("unchecked")
 	public byte[] compress(final byte[] uncompressed, final int quality) {
-		if (quality < 0 || quality > 11) {
-			throw new IllegalArgumentException(quality + " - Quality range is 0 to 11");
+		if (quality < MIN_QUALITY || quality > MAX_QUALITY) {
+			throw new IllegalArgumentException(quality + " - quality range is " + MIN_QUALITY + " to " + MAX_QUALITY);
 		}
-		final Array<Number> array = (Array<Number>) instance.compressArray(Array.ofNative(toIntegerObject(uncompressed)), quality);
+		final Array<Number> array = (Array<Number>) brotli.compressArray(Array.ofNative(toIntegerObject(uncompressed)), quality);
 
-		final byte[] compressed = new byte[array.length];
+		final int length = array.length;
+		final byte[] compressed = new byte[length];
 		int i = 0;
 		final ArrayIterator<Number> iterator = (ArrayIterator<Number>) array.iterator();
-		while (iterator.hasNext()) {
+		while (i < length) {
 			compressed[i++] = iterator.next().byteValue();
 		}
 
 		return compressed;
 	}
 
+	/**
+	 * Decompress a byte array compressed using <b>Brotli</b> compression
+	 * format.
+	 * 
+	 * @param compressed the compressed byte array
+	 * @return the decompressed byte array
+	 */
 	@SuppressWarnings("unchecked")
 	public byte[] decompress(final byte[] compressed) {
-		final Array<Number> array = (Array<Number>) instance.decompressArray(Array.ofNative(toIntegerObject(compressed)));
+		final Array<Number> array = (Array<Number>) brotli.decompressArray(Array.ofNative(toIntegerObject(compressed)));
 
-		final byte[] decompressed = new byte[array.length];
+		final int length = array.length;
+		final byte[] decompressed = new byte[length];
 		int i = 0;
 		final ArrayIterator<Number> iterator = (ArrayIterator<Number>) array.iterator();
-		while (iterator.hasNext()) {
+		while (i < length) {
 			decompressed[i++] = iterator.next().byteValue();
 		}
 

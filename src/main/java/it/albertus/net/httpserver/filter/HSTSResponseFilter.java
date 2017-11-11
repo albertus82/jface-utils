8a5eedd588e46273336b76c165b8b8e23c27c698
Alberto <albertus82@gmail.com>
2017-11-11 23:38:31 +0100

    HSTSResponseFilter

--- src/main/java/it/albertus/net/httpserver/filter/HSTSResponseFilter.java
+++ src/main/java/it/albertus/net/httpserver/filter/HSTSResponseFilter.java
@@ -5,32 +5,100 @@ import java.io.IOException;
 import com.sun.net.httpserver.Filter;
 import com.sun.net.httpserver.HttpExchange;
 
+import it.albertus.util.ISupplier;
+
+/**
+ * This filter adds a HTTP {@code Strict-Transport-Security} response header to
+ * every HTTP response.
+ */
 @SuppressWarnings("restriction")
 public class HSTSResponseFilter extends Filter {
 
 	private static final String HEADER_KEY = "Strict-Transport-Security";
 
-	private final String headerValue;
+	private ISupplier<Integer> maxAge;
+	private ISupplier<Boolean> includeSubDomains;
+	private ISupplier<Boolean> preload;
 
-	public HSTSResponseFilter(final int maxAge, final boolean includeSubDomains, final boolean preload) {
-		final StringBuilder value = new StringBuilder("max-age=").append(maxAge);
-		if (includeSubDomains) {
-			value.append("; includeSubDomains");
-		}
-		if (preload) {
-			value.append("; preload");
-		}
-		this.headerValue = value.toString();
+	private String headerValue;
+
+	/**
+	 * Creates the filter using the provided suppliers for the generation of the
+	 * {@code Strict-Transport-Security} header value.
+	 * 
+	 * @param maxAge the supplier of the {@code max-age} directive value
+	 * @param includeSubDomains the supplier of the {@code includeSubDomains}
+	 *        option switch (the option is omitted when the supplier returns
+	 *        {@code false})
+	 * @param preload the supplier of the {@code preload} option switch (the
+	 *        option is omitted when the supplier returns {@code false})
+	 */
+	public HSTSResponseFilter(final ISupplier<Integer> maxAge, final ISupplier<Boolean> includeSubDomains, final ISupplier<Boolean> preload) {
+		this.maxAge = maxAge;
+		this.includeSubDomains = includeSubDomains;
+		this.preload = preload;
 	}
 
+	/**
+	 * Creates the filter using the provided values for the generation of the
+	 * {@code Strict-Transport-Security} header value.
+	 * 
+	 * @param maxAge the {@code max-age} directive value
+	 * @param includeSubDomains the {@code includeSubDomains} option switch (the
+	 *        option is omitted when the supplier returns {@code false})
+	 * @param preload the {@code preload} option switch (the option is omitted
+	 *        when the supplier returns {@code false})
+	 */
+	public HSTSResponseFilter(final int maxAge, final boolean includeSubDomains, final boolean preload) {
+		this(new ISupplier<Integer>() {
+			@Override
+			public Integer get() {
+				return maxAge;
+			}
+		}, new ISupplier<Boolean>() {
+			@Override
+			public Boolean get() {
+				return includeSubDomains;
+			}
+		}, new ISupplier<Boolean>() {
+			@Override
+			public Boolean get() {
+				return preload;
+			}
+		});
+	}
+
+	/**
+	 * Creates the filter using the provided value for the
+	 * {@code Strict-Transport-Security} header.
+	 * 
+	 * @param headerValue the value that will be used for the
+	 *        {@code Strict-Transport-Security} header.
+	 */
 	public HSTSResponseFilter(final String headerValue) {
 		this.headerValue = headerValue;
 	}
 
 	@Override
 	public void doFilter(final HttpExchange exchange, final Chain chain) throws IOException {
-		exchange.getResponseHeaders().set(HEADER_KEY, headerValue);
+		exchange.getResponseHeaders().set(HEADER_KEY, getHeaderValue());
 		chain.doFilter(exchange);
+	}
+
+	private String getHeaderValue() {
+		if (headerValue != null) {
+			return headerValue;
+		}
+		else {
+			final StringBuilder value = new StringBuilder("max-age=").append(maxAge.get().intValue());
+			if (includeSubDomains.get()) {
+				value.append("; includeSubDomains");
+			}
+			if (preload.get()) {
+				value.append("; preload");
+			}
+			return value.toString();
+		}
 	}
 
 	@Override

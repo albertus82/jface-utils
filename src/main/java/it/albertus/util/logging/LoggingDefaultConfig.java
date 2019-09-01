65a31d01f6f07d4e06199244ffbf7bbd9dfdea66
Alberto <albertus82@users.noreply.github.com>
2019-09-01 16:28:31 +0200

    Refactoring.

--- src/main/java/it/albertus/util/logging/LoggingDefaultConfig.java
+++ src/main/java/it/albertus/util/logging/LoggingDefaultConfig.java
@@ -4,7 +4,7 @@ import java.util.logging.Level;
 
 public abstract class LoggingDefaultConfig implements ILoggingConfig {
 
-	public static final String DEFAULT_LOG_FORMAT_FILE = "%1$td/%1$tm/%1$tY %1$tH:%1$tM:%1$tS.%tL %4$s %3$s - %5$s%6$s%n";
+	public static final String DEFAULT_LOG_FORMAT_FILE = LoggingSupport.DEFAULT_FORMAT;
 	public static final Level DEFAULT_LOGGING_LEVEL = Level.INFO;
 	public static final boolean DEFAULT_LOGGING_FILES_ENABLED = true;
 	public static final int DEFAULT_LOGGING_FILES_LIMIT_KB = 1024;

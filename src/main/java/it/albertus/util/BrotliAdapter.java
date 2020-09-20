7999cfe117223d2fe8cf6d8ce8ef4fe489b206ef
Alberto <albertus82@users.noreply.github.com>
2020-09-20 13:15:13 +0200

    Brotli decompression with official Java library from Google.

--- src/main/java/it/albertus/util/BrotliAdapter.java
+++ src/main/java/it/albertus/util/BrotliAdapter.java
@@ -1,5 +1,12 @@
 package it.albertus.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.brotli.dec.BrotliInputStream;
+
 import _Array.ArrayIterator;
 import haxe.lang.EmptyObject;
 import haxe.root.Array;
@@ -19,27 +26,43 @@ public class BrotliAdapter {
 	public static final int MAX_QUALITY = 11;
 
 	private final Brotli brotli;
+	private final byte[] customDictionary;
 
 	/** Construct a {@code BrotliAdapter} using the default dictionary. */
 	public BrotliAdapter() {
 		brotli = new Brotli(EmptyObject.EMPTY);
+		customDictionary = null;
 	}
 
 	/**
 	 * Construct a {@code BrotliAdapter} using a custom dictionary.
 	 * 
 	 * @param dictionaryPath the full path of the dictionary file.
+	 * 
+	 * @throws IOException if a read error occurs.
 	 */
-	public BrotliAdapter(final String dictionaryPath) {
+	public BrotliAdapter(final String dictionaryPath) throws IOException {
 		brotli = new Brotli(dictionaryPath);
+		FileInputStream fis = null;
+		ByteArrayOutputStream baos = null;
+		try {
+			fis = new FileInputStream(dictionaryPath);
+			baos = new ByteArrayOutputStream();
+			IOUtils.copy(fis, baos, 8192);
+		}
+		finally {
+			IOUtils.closeQuietly(baos, fis);
+		}
+		customDictionary = baos.toByteArray();
 	}
 
 	/**
 	 * Compress a byte array using <b>Brotli</b> compression format.
 	 * 
 	 * @param uncompressed the byte array to compress
-	 * @param quality the compression quality, valid range is
-	 *        {@link #MIN_QUALITY} to {@link #MAX_QUALITY}
+	 * @param quality      the compression quality, valid range is
+	 *                     {@value #MIN_QUALITY} to {@value #MAX_QUALITY}
+	 * 
 	 * @return the compressed byte array.
 	 */
 	@SuppressWarnings("unchecked")
@@ -51,15 +74,28 @@ public class BrotliAdapter {
 	}
 
 	/**
-	 * Decompress a byte array compressed using <b>Brotli</b> compression
-	 * format.
+	 * Decompress a byte array compressed using <b>Brotli</b> compression format.
 	 * 
 	 * @param compressed the compressed byte array
+	 * 
 	 * @return the decompressed byte array
+	 * 
+	 * @throws IOException in case of corrupted data or source stream problems
 	 */
-	@SuppressWarnings("unchecked")
-	public byte[] decompress(final byte[] compressed) {
-		return toNative((Array<Number>) brotli.decompressArray(Array.ofNative(toIntegerObject(compressed))));
+	public byte[] decompress(final byte[] compressed) throws IOException {
+		ByteArrayInputStream bais = null;
+		ByteArrayOutputStream baos = null;
+		BrotliInputStream bis = null;
+		try {
+			bais = new ByteArrayInputStream(compressed);
+			baos = new ByteArrayOutputStream();
+			bis = new BrotliInputStream(bais, BrotliInputStream.DEFAULT_INTERNAL_BUFFER_SIZE, customDictionary);
+			IOUtils.copy(bis, baos, 8192);
+		}
+		finally {
+			IOUtils.closeQuietly(baos, bis, bais);
+		}
+		return baos.toByteArray();
 	}
 
 	@SuppressWarnings("unchecked")

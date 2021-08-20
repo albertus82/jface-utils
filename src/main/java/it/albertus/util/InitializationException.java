6223ef487c9125ef76973c217767bc1f08e66434
Alberto <albertus82@users.noreply.github.com>
2021-08-20 20:28:35 +0200

    Added more constructors to InitializationException.

--- src/main/java/it/albertus/util/InitializationException.java
+++ src/main/java/it/albertus/util/InitializationException.java
@@ -2,7 +2,17 @@ package it.albertus.util;
 
 public class InitializationException extends RuntimeException {
 
-	private static final long serialVersionUID = 6790615029242056177L;
+	private static final long serialVersionUID = 7424607632683923770L;
+
+	public InitializationException() {}
+
+	public InitializationException(final String message) {
+		super(message);
+	}
+
+	public InitializationException(final Throwable cause) {
+		super(cause);
+	}
 
 	public InitializationException(final String message, final Throwable cause) {
 		super(message, cause);
